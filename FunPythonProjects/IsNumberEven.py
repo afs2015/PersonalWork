9792599141ae486bc8d63aaa4be83ad218ab90a4
Andrew Selzer <andrewfselzer@gmail.com>
2013-10-01 12:19:24 -0400

    Adding IsNumberEven.py
    Adds a program that determines if a number is even or not.

---
+++ FunPythonProjects/IsNumberEven.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Author: Andrew Selzer
+# Purpose: Simple function to determine if a number is even.
+
+print ("Type is_even(number) to use this program")
+
+def is_even(x):
+    if x % 2 == 1:
+        return False
+    else:
+        return True
