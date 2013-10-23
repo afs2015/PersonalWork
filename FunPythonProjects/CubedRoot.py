bd4851dbbb926a8bf38c53f7e2a2d93065ba1bfb
Andrew Selzer <andrewfselzer@gmail.com>
2013-10-23 16:55:02 -0400

    Updated CubedRoot.py
    Added the ability to handle negative numbers.

--- FunPythonProjects/CubedRoot.py
+++ FunPythonProjects/CubedRoot.py
@@ -7,9 +7,9 @@ print ("Type cubedRoot(number) to use this program.")
 
 def cubedRoot(num):
 	ans = 0
-	while (ans**3 < num):
+	while (ans**3 < abs(num)):
 		ans += 1
-	if (ans**3 != num):
+	if (ans**3 != abs(num)):
 		return str(num) + " is not a perfect cube"
 	else:
 		return "The cubed root is " + str(ans)
