e0bc95fbf5a65572e9ed60a18f9377b7156ad47c
Andrew Selzer <andrewfselzer@gmail.com>
2013-11-07 18:14:48 -0500

    Updated PigLatinTranslator.py
    Made code more efficient

--- FunPythonProjects/PigLatinTranslator.py
+++ FunPythonProjects/PigLatinTranslator.py
@@ -9,7 +9,7 @@ original = input('Enter a word:')
 if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
-    if first == 'a' or first == 'e' or first == 'i' or first == 'o' or first == 'u':
+    if first in 'aeiou':
         new_word = word + 'ay'
         print (new_word)
     else:
